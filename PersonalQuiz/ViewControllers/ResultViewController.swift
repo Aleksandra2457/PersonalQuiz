53c71613fd8ba53fe3f28429b71dd17787c93e9e
Aleksandra Moiseenko <lesovskaya.aleks@mail.ru>
2022-03-16 14:49:15 +0300

    Initial Commit

---
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz
+//
+//  Created by Alexey Efimov on 14.03.2022.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    // MARK: - Private Properties
+    private var responses: [Answer] = []
+    
+    // 1. Передать массив с ответами на экран с результатами
+    // 2. Определить наиболее часто встречающийся тип животного
+    // 3. Отобразить результаты в соответствии с этим животным
+    // 4. Избавиться от кнопки возврата назад на экране результатов
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
